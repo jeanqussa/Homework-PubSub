d69389ef2b9bca6056e1580a77936c77990d3a6e
jeanqussa <jean.qussa@gmail.com>
2019-03-21 21:43:37 +0200

    Add WeatherEventArgs and WeatherPublisher classes

--- Program.cs
+++ Program.cs
@@ -1,12 +1,37 @@
 ﻿using System;
 
-namespace PubSub
+namespace Homework.PubSub
 {
+    public class WeatherEventArgs : EventArgs {
+        public double Temperature { get; set; }
+        public double Humidity { get; set; }
+
+        public WeatherEventArgs(double temperature, double humidity) {
+            Temperature = temperature;
+            Humidity = humidity;
+        }
+    }
+
+    public class WeatherPublisher {
+        public event EventHandler<WeatherEventArgs> WeatherDataReceived;
+
+        public virtual void OnWeatherDataReceived(WeatherEventArgs args) {
+            if (WeatherDataReceived != null) {
+                WeatherDataReceived(this, args);
+            }
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            WeatherPublisher weatherPub = new WeatherPublisher();
+
+            weatherPub.WeatherDataReceived += (sender, e) => Console.WriteLine("Temperature is " + e.Temperature);
+
+            weatherPub.OnWeatherDataReceived(new WeatherEventArgs(10.0, 14.3));
+            weatherPub.OnWeatherDataReceived(new WeatherEventArgs(12.2, 5.0));
         }
     }
 }
