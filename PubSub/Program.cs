b77c817873bbf761ca02c0dd933f740c09c3b1c0
jeanqussa <jean.qussa@gmail.com>
2019-03-22 00:19:45 +0200

    Add comments to Program.cs

--- PubSub/Program.cs
+++ PubSub/Program.cs
@@ -6,13 +6,22 @@ namespace Homework.PubSub
     {
         static void Main(string[] args)
         {
+            // PubSub object for tracking weather data in multiple cities
             var weatherPubSub = new PubSub<WeatherEventArgs>();
 
+            // We subscribe to the cities of "Damascus" and "Homs" and print the temperature or
+            // humidity everytime a new WeatherEventArgs is published to one of these cities
             weatherPubSub.Subscribe("Damascus", (sender, e) => Console.WriteLine("Temperature in Damascus is " + e.Temperature));
-            weatherPubSub.Subscribe("Homs", (sender, e) => Console.WriteLine("Temperature in Homs is " + e.Temperature));
+            weatherPubSub.Subscribe("Homs", (sender, e) => Console.WriteLine("Humidity in Homs is " + e.Humidity));
 
-            weatherPubSub.Publish("Damascus", new WeatherEventArgs(10.0, 14.3));
-            weatherPubSub.Publish("Homs", new WeatherEventArgs(12.2, 5.0));
+            // We publish one WeatherEventArgs to each of "Damascus" and "Homs"
+            weatherPubSub.Publish("Damascus", new WeatherEventArgs(10.0, 61.7));
+            weatherPubSub.Publish("Homs", new WeatherEventArgs(12.2, 51.5));
+
+            // Prints:
+            //  Temperature in Damascus is 10
+            //  Humidity in Homs is 51.5
+
         }
     }
 }
